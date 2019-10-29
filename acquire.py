f39530252e38670d972dec8c33cb7a96942563ea
Misty <ms.misty.garcia@gmail.com>
2019-10-29 16:56:33 -0500

    query in acquire file

--- acquire.py
+++ acquire.py
@@ -2,6 +2,15 @@ import pandas as pd
 
 import util
 
-def get_data(query, db):
+def get_telco_data():
+    query = """
+    SELECT * 
+    FROM customers
+    JOIN internet_service_types USING (internet_service_type_id)
+    JOIN contract_types USING (contract_type_id)
+    JOIN payment_types USING (payment_type_id)
+    """
+    db = "telco_churn"
+
     return pd.read_sql(query, util.get_url(db))
 
